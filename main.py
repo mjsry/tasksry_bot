30a358e55ee43627c6d55e6898287e5d0f577ab4
mohammad javad <mjsoory@gmail.com>
2025-02-16 07:45:06 +0330

    first commit

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from telegram import KeyboardButton, ReplyKeyboardMarkup
 
 tk = config('Token')
 tasks = {}
-task_counter = 1
+
 user_states = {}
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -28,15 +28,18 @@ async def add_task(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(txt)
 
 async def save_task(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    global task_counter
     user_id = update.effective_chat.id
     if user_states.get(user_id) == 'adding_task':
 
         task_text = update.effective_message.text.strip()
         if task_text :
+            if user_id not in tasks:
+                tasks[user_id] = {}
+
+            task_counter = max(tasks[user_id].keys(), default=0) + 1
             new_tasks = [task.strip() for task in task_text.splitlines() if task.strip()]
             for task in new_tasks:
-                tasks[task_counter] = {'task': task, 'status': 'not done'}
+                tasks[user_id][task_counter] = {'task': task, 'status': 'not done'}
                 task_counter += 1
 
             user_states.pop(user_id)
@@ -48,20 +51,23 @@ async def save_task(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     elif user_states.get(user_id) == 'done task':
         task_number_done = update.effective_message.text.strip()
-        if task_number_done.isdigit() and int(task_number_done) in tasks :
+        if task_number_done.isdigit() :
             task_number_done = int(task_number_done)
-            tasks[task_number_done]['status'] = 'done'
-            txt = '✔Task done'
-            await update.message.reply_text(txt)
+            if task_number_done in tasks[user_id] :
+                tasks[user_id][task_number_done]['status'] = 'done'
+                user_states.pop(user_id)
+                txt = '✔Task done'
+                await update.message.reply_text(txt)
     else:
         txt = '✖️Please use the buttons'
         await update.message.reply_text(txt)
 
 
 async def show_tasks(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    if tasks :
+    user_id = update.effective_chat.id
+    if user_id in tasks and tasks[user_id]:
 
-        task_list = "\n".join([f'{task_number}.{task["task"]}{"✔" if task["status"] == "done" else ""}' for task_number, task in tasks.items()])
+        task_list = "\n".join([f'{task_number}.{task["task"]}{"✔" if task["status"] == "done" else ""}' for task_number, task in tasks[user_id].items()])
         txt = '📋your tasks:'
         await update.message.reply_text(f'{txt} \n\n {task_list}')
     else:
@@ -70,17 +76,18 @@ async def show_tasks(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 async def done_task(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user_id = update.effective_chat.id
-    user_states[user_id] = 'done task'
-    if tasks:
+
+    if user_id in tasks and tasks[user_id]:
+        user_states[user_id] = 'done task'
         task_list = "\n".join(
             [f'{task_number}. {task["task"]} {"✔" if task["status"] == "done" else ""}' for task_number, task in
-             tasks.items()])
+             tasks[user_id].items()])
+        txt = '📝Enter the completed task number'
+        txt1 = '📋your tasks:'
+        await update.message.reply_text(f'{txt1} \n\n {task_list} \n\n {txt}')
     else:
-        task_list = ''
-
-    txt = '📝Enter the completed task number'
-    txt1 = '📋your tasks:'
-    await update.message.reply_text(f'{txt1} \n\n {task_list} \n\n {txt}')
+        txt = '📭 No tasks found!'
+        await update.message.reply_text(txt)
 
 async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
     txt = '''با استفاده از add task میتونید تسک اضافه کنید اگه چند تا تسک میخواید اضافه کنید هر کدوم رو توی یک خط بنویسید
