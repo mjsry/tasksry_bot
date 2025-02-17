b319afaf471426606a67c7604f206e3f600b9b61
mohammad javad <mjsoory@gmail.com>
2025-02-17 17:47:21 +0330

    استفاده از دیتا بیس

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ db = mysql.connector.connect(
         user=url.username,
         password=url.password,
         database=url.path[1:],
-        port=3306
+        port=url.port
     )
 cursor = db.cursor()
 def creat_table():
