4c186230b2f491c5c28888dc8f6d36e70b1b8bd2
mohammad javad <mjsoory@gmail.com>
2025-02-19 18:55:48 +0330

    fix

--- app.py
+++ app.py
@@ -401,10 +401,6 @@ async def main():
     await app.run_polling()
 
 if __name__ == '__main__':
-    try:
-        loop = asyncio.get_event_loop()
-        loop.run_until_complete(main())
-    except RuntimeError:
-        loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(loop)
-        loop.run_until_complete(main())
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(main())
